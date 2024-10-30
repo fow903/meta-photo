8f76d5e201b18cbea3944da73e2a83c4309252c5
Edwin de los Santos <edwin2delossantos@gmail.com>
2024-10-30 08:07:51 -0400

    Refactor: Update app.module.ts to include ConfigModule and HttpModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import { HttpModule } from "@nestjs/axios";
+import { UsersController } from "./controllers/user/users.controller";
+import { UsersService } from "./services/users/users.service";
+import { PhotosController } from "./controllers/photos/photos.controller";
+import { PhotosService } from "./services/photos/photos.service";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		HttpModule,
+	],
+	controllers: [AppController, UsersController, PhotosController],
+	providers: [AppService, UsersService, PhotosService],
 })
 export class AppModule {}
