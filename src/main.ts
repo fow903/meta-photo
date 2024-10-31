c6d218a3452e787a1f83b521ba0e415813b7275b
Edwin de los Santos <edwin2delossantos@gmail.com>
2024-10-31 11:08:14 -0400

    Refactor: Remove unused app.controller.spec.ts file and enable CORS in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
-    credentials: true
-  });
+	app.enableCors({
+		origin: '*',
+		methods: 'GET,POST,PUT,DELETE',
+		allowedHeaders: 'Content-Type,Authorization',
+		credentials: true,
+	});
 
-  await app.listen(3000);
+	await app.listen(3000);
 }
 bootstrap();
