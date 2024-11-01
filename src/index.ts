9a387ea829edc783fc1c45358dd3bfb156d23346
Edwin de los Santos <edwin2delossantos@gmail.com>
2024-10-31 21:11:28 -0400

    Refactor: Update import statements in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { onRequest } from 'firebase-functions/v2/https';
-import { server } from './main';
+import { onRequest } from "firebase-functions/v2/https";
+import { server } from "./main";
 
 export const api = onRequest(server);
